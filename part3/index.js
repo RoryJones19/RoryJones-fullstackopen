c57a837a15b9690ef9d2f73f92cf76954368f7a0
RoryJones19 <rjones382001@yahoo.com>
2025-02-10 11:57:57 -0600

    saving before exercises

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -36,6 +38,33 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).send('ayo').end()
   }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
